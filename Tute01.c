06e703a7e633cdfae087d6f6fb3cf68117fc9914
NAVODYABGS <128354320+NAVODYABGS@users.noreply.github.com>
2023-03-23 14:07:49 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
+   //Declare the variables
+   int mark1, mark2; 
+   float avg;
+   
+   //Getting the user inputs
+   printf("Enter the mark 1 : ");
+   scanf("%d", &mark1);
+   
+   printf("Enter the mark 2 : ");
+   scanf("%d", &mark2);
+   
+   avg = (mark1 + mark2) / 2; //Calculate the average marks
+   printf("Average marks is %.2f\n", avg); //Display the average marks
   
   return 0;
-}
+}//End of function main
 
